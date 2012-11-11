c861c29d8c5261326539ad75904b34801d0215a8
Alexandr Korsak <alex.korsak@gmail.com>
2012-11-11 16:11:35 +0300

    Add dialog dependecy

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-module.exports = function() {
-  var $ = require('component-zepto');
+var $ = require('zepto-component');
 
-  var Feedbacks = function(options) {
-    if (typeof options.api_key === 'undefined') return;
-    this.api_key = options.api_key;
+var Feedbacks = function(options) {
+  if (typeof options.api_key === 'undefined') return;
+  this.api_key = options.api_key;
 
-    this.parse(options)
+  this.parse(options)
 
-    this.html();
-    this.events();
-  };
-
-  Feedbacks.prototype.parse = function(options) {
-    this.label = options.label || "Feedback";
-  };
-
-  Feedbacks.prototype.html = function() {
-    var container = $("<div id='feedbacks'>");
-    var button = $("<a href='#'>" + this.label + "</a>");
-
-    container.append(button);
-    $(document).append(container);
-  };
-
-  Feedbacks.prototype.events = function() {
-  };
-
-  return Feedbacks;
+  this.html();
+  this.events();
 };
+
+Feedbacks.prototype.parse = function(options) {
+  this.label = options.label || "Feedback";
+  this.class = options.class || "button1";
+};
+
+Feedbacks.prototype.html = function() {
+  this.container = $("<div id='feedbacks' class='" + this.class + "'>");
+  this.button = $("<span>" + this.label + "</span>");
+
+  this.container.append(this.button);
+  $('body').append(this.container);
+};
+
+Feedbacks.prototype.events = function() {
+  $(this.button).click(function(event) {
+    event.preventDefault();
+
+    console.log('test');
+  });
+};
+
+module.exports = Feedbacks;
 
